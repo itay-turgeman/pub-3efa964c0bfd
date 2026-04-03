aeed5b321d9161b8989c86eee0018c4b946ed9de
Itai Turgeman <itayspacecowboy@gmail.com>
2026-04-03 11:47:58 -0700

    More pink — red+bright slots now pink

--- atlas-blink.js
+++ atlas-blink.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#1E1E2E", "#E06C75", "#98C379", "#E5C07B", "#61AFEF", "#FF79C6", "#80CBC4", "#ABB2BF", "#545862", "#E06C75", "#98C379", "#E5C07B", "#61AFEF", "#FF79C6", "#5DE4C7", "#ECEFF4"]);
-t.prefs_.set('foreground-color', "#80CBC4");
-t.prefs_.set('background-color', "#1E1E2E");
+t.prefs_.set('color-palette-overrides',["#1e1e2e", "#ff79c6", "#98c379", "#e5c07b", "#61afef", "#ff79c6", "#80cbc4", "#abb2bf", "#545862", "#ff92d0", "#98c379", "#e5c07b", "#61afef", "#ff92d0", "#5de4c7", "#eceff4"]);
+t.prefs_.set('foreground-color', "#80cbc4");
+t.prefs_.set('background-color', "#1e1e2e");
 t.prefs_.set('cursor-color', 'rgba(255,121,198,0.5)');
